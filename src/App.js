0e5970b7a55c1cf053191674b265b29b5e496542
RobertMaina <robertmaina254@gmail.com>
2021-10-03 13:43:08 +0300

    create components folder with Panel component and communicate the items prop from App component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
+import Panel from './components/Panel'
+
 function App() {
+  const items = [
+    {
+        question: 'what is React?',
+        answer: 'React is a frontend javascript library'
+    },
+    {
+        question: 'why use React?',
+        answer: 'React is popular among engineers'
+    },
+    {
+        question: 'how do you use React?',
+        answer: 'by building components'
+    }
+]
+
   return (
-    <div className="App">
+    <div>
+      <Panel items= {items} />
     </div>
   );
 }
---
+++ src/components/Panel.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Panel = ({ items }) => {
+
+    return (
+        <div>
+            <h1>{ items.length }</h1>
+        </div>
+    )
+}
+
+export default Panel
