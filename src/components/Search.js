7ade71f7cc2d667f7c4d73b06460b2917e4da37f
RobertMaina <robertmaina254@gmail.com>
2021-10-06 21:27:20 +0300

    add axios dependency and useEffect to fetch wikisearch api

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,9 +1,38 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function Search() {
+
+    const [term, setTerm] = useState("");
+
+    useEffect(() =>{
+        const search = async () => {    // recommmended approach to use async inside useEffect hook
+            await axios.get('https://en.wikipedia.org/w/api.php', {
+                params: {
+                    action: 'query',
+                    list: 'search',
+                    origin: '*',
+                    format: 'json',
+                    srsearch: term
+                }
+            });
+        };
+        search()
+
+    }, [term]) // whenever we re-render the component and term has changed, run the arrow function
+
     return (
         <div>
-            
+            <div className= "ui form">
+                <div className= "field">
+                    <label>Search</label>
+                    <input 
+                        className= "input"
+                        value= {term}
+                        onChange= {e => setTerm(e.target.value)}
+                    />
+                </div>
+            </div>
         </div>
     )
 }
